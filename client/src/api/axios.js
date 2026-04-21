984a9abc3cb75f5d94e68e12bf27270bc5c4f068
AmishaPatel7476 <n12273171@qut.edu.au>
2026-04-21 11:44:46 +1000

    updated files 11:44-21/04

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_API,
+  baseURL: import.meta.env.VITE_BASE_API || "http://localhost:5000/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
